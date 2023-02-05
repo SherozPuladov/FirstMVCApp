0e54d8fbbe3e99477443bc70fcdda65fbb3112ab
Sheroz Puladov <108729229+SherozPuladov@users.noreply.github.com>
2023-02-05 21:31:30 +0500

    First Commit

---
+++ FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using FirstMVCApp.Models;
+
+namespace FirstMVCApp.Controllers;
+
+public class HelloWorldController : Controller
+{
+    private static List<DogViewModel> dogs = new List<DogViewModel>();
+
+    public IActionResult Index()
+    {
+        return View(dogs);
+    }
+
+    public IActionResult Hello()
+    {
+        return View();
+    }
+
+    public IActionResult Create()
+    {
+        var dogVm = new DogViewModel();
+        return View(dogVm);
+    }
+
+    public IActionResult CreateDog(DogViewModel dogViewModel)
+    {
+        dogs.Add(dogViewModel);
+        return RedirectToAction(nameof(Index));
+    }
+}
